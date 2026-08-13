69fb4ace68c5161832331ca9a2bfe2ef2edf8fbe
Falck <ffalcrum@gmail.com>
2026-08-14 01:21:28 +0800

    Реализовал основные команды

--- Wc/main.go
+++ Wc/main.go
@@ -27,7 +27,10 @@ func main() {
 	} else {
 		content = parseArgs()
 	}
-	fmt.Printf(" Кол. слов: %d \n Кол. строк: %d \n Кол.байт: %d \n", content.numberWords, content.numberLine, content.numberBytes)
+
+	if content.numberLine != 0 && content.numberWords != 0 && content.numberBytes != 0 {
+		fmt.Printf(" Кол. слов: %d \n Кол. строк: %d \n Кол.байт: %d \n", content.numberWords, content.numberLine, content.numberBytes)
+	}
 }
 
 /*
@@ -63,48 +66,119 @@ func parseArgs() Content {
 
 	content := Content{}
 
-	for _, arg := range os.Args {
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println(
+			"-w 	Отобразить колличество слов в объекте \n" +
+				"-l		Вывести колличество строк в объекте\n" +
+				"-m		Показать количество символов в объекте\n" +
+				"-c		Отобразить размер объекта в байтах\n\n" +
+				"Без параметров: Если аргументов нету то подсчитывается все вышепречисленное из свободного ввод в командную строку." +
+				"Если указан путь к файлу: подсчитывается все вышепречисленное для файла")
+		return Content{}
+	}
 
-		if arg == "-h" {
-			fmt.Println(
-				"-w 	Отобразить колличество слов в объекте \n" +
-					"-l		Вывести колличество строк в объекте\n" +
-					"-m		Показать количество символов в объекте\n" +
-					"-c		Отобразить размер объекта в байтах\n\n" +
-					"Без параметров: Если аргументов нету то подсчитывается все вышепречисленное из свободного ввод в командную строку." +
-					"Если указан путь к файлу: подсчитывается все вышепречисленное для файла")
+	file := OpenFile(os.Args[len(os.Args)-1])
 
+	defer file.Close()
+
+	for _, arg := range os.Args[1:] {
+
+		if arg == "-l" {
+			lineNumber := CalculateLineOfFile(file)
+			content.numberLine = lineNumber
+			break
+		} else if arg == "-w" {
+			numberWords := CalculateWordsOfFile(file)
+			content.numberWords = numberWords
+			break
+		} else if arg == "-m" {
+
+		} else if arg == "-c" {
+			bytesNumber := CalculateFileOfBytes(file)
+			content.numberBytes += bytesNumber
+			break
 		} else {
-			content = readFile(arg)
+			content = AnalyzingAllFile(file)
+			break
 		}
 	}
 	return content
 }
 
-func readFile(fileName string) Content {
+func AnalyzingAllFile(file *os.File) Content {
+
+	scaner := bufio.NewScanner(file)
+
+	lineNumber := 0
+	wordsNumber := 0
+	bytesNumber := CalculateFileOfBytes(file)
+
+	for scaner.Scan() {
+		lineNumber++
+		wordsNumber += len(strings.Fields(scaner.Text()))
+	}
+
+	return Content{
+		numberLine:  lineNumber,
+		numberWords: wordsNumber,
+		numberBytes: bytesNumber,
+	}
+}
+
+func OpenFile(fileName string) *os.File {
+
 	file, err := os.Open(fileName)
-	content := Content{}
 
 	if err != nil {
 		log.Fatal(err, "Файл не был найден: "+fileName)
-		os.Exit(1)
 	}
 
-	defer file.Close()
+	return file
+}
 
+/*
+Вычисление байтов в файле
+*/
+func CalculateFileOfBytes(file *os.File) int {
+	info, err := file.Stat()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return int(info.Size())
+}
+
+/*
+Вычислить слова из файла
+*/
+func CalculateWordsOfFile(file *os.File) int {
 	scaner := bufio.NewScanner(file)
-	strBuilder := strings.Builder{}
+	wordsNumber := 0
 
 	for scaner.Scan() {
-		strBuilder.WriteString(scaner.Text())
-		strBuilder.WriteString(" ")
-		content.numberLine++
-		content.numberBytes += len(scaner.Bytes())
+		wordsNumber += len(strings.Fields(scaner.Text()))
 	}
 
-	words := strings.Fields(strBuilder.String())
+	return wordsNumber
+}
 
-	content.numberWords = len(words)
+/*
+Вычислить количество строк
+*/
+func CalculateLineOfFile(file *os.File) int {
+	data, err := os.ReadFile(file.Name())
 
-	return content
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text := string(data)
+	lineNumber := strings.Count(string(data), "\n")
+
+	if len(text) > 0 && text[len(text)-1] != '\n' {
+		lineNumber++
+	}
+
+	return lineNumber
 }
